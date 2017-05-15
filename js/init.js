7ebdf65db5ba8670d24c35575e92e381f488f6b8
Alex Autem <autem.alex@gmail.com>
2017-05-15 12:04:53 -0400

    Update portfolio styling to use Materialize

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy({scrollOffset: 55});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
