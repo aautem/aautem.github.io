fad3db66f68ab9b2306f4c4b615fa76ba9546625
Alex Autem <autem.alex@gmail.com>
2017-05-16 00:26:00 -0400

    Add social links and animations to site

--- js/init.js
+++ js/init.js
@@ -5,5 +5,59 @@
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy({scrollOffset: 55});
 
+    var options = [
+      {selector: '.full-stack', offset: 0, callback: function(el) {
+        setTimeout(function() {
+          Materialize.showStaggeredList($(el));
+        }, 250);
+      } },
+      {selector: '.personal', offset: 0, callback: function(el) {
+        setTimeout(function() {
+          Materialize.showStaggeredList($(el));
+        }, 500);
+      } },
+      {selector: '#savvyshopper-list', offset: 150, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#savvyshopper-pic', offset: 200, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+      {selector: '#sandpiper-list', offset: 150, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#sandpiper-pic', offset: 200, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+      {selector: '#metis-list', offset: 150, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#metis-pic', offset: 200, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+      {selector: '#bracketbuilder-list', offset: 150, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#bracketbuilder-pic', offset: 200, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+      {selector: '#hangman-list', offset: 150, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#hangman-pic', offset: 200, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+      {selector: '#viewtube-list', offset: 150, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#viewtube-pic', offset: 200, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+      {selector: '.resume-links', offset: 150, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+    ];
+
+    Materialize.scrollFire(options);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
