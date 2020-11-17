7c693c66719deeccba5ed4d6d32fa3af0df23466
Alex Autem <autem.alex @gmail.com>
2020-11-16 21:06:51 -0600

    Add Connect and Work Order Generator projects

--- js/init.js
+++ js/init.js
@@ -16,6 +16,54 @@
           Materialize.showStaggeredList($(el));
         }, 500);
       } },
+      {selector: '#connect-list', offset: 150, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#connect-pic', offset: 200, callback: function(el) {
+        Materialize.fadeInImage($(el));
+
+        let index = 0;
+        const link = $('#connect-image-link');
+        const image = $('#connect-image-link img');
+        const imgSrcs = [
+          'images/connect/exam_evaluation.png',
+          'images/connect/add_preventative.png',
+          'images/connect/scrollable_table.png',
+          'images/connect/dashboard.png',
+          'images/connect/medical_exam.png',
+        ];
+
+        // Loop through images
+        setInterval(() => {
+          const src = imgSrcs[index];
+          link.attr('href', `/${src}`);
+          image.attr('src', src);
+          index = index === 4 ? 0 : index + 1;
+        }, 3500);
+      } },
+      {selector: '#wog-list', offset: 150, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#wog-pic', offset: 200, callback: function(el) {
+        Materialize.fadeInImage($(el));
+
+        let index = 0;
+        const link = $('#wog-image-link');
+        const image = $('#wog-image-link img');
+        const imgSrcs = [
+          'images/wog/new_equipment.jpg',
+          'images/wog/work_order_list.jpg',
+          'images/wog/equipment_list.jpg',
+        ];
+
+        // Loop through images
+        setInterval(() => {
+          const src = imgSrcs[index];
+          link.attr('href', `/${src}`);
+          image.attr('src', src);
+          index = index === 2 ? 0 : index + 1;
+        }, 3500);
+      } },
       {selector: '#cointrail-list', offset: 150, callback: function(el) {
         Materialize.showStaggeredList($(el));
       } },
@@ -38,12 +86,6 @@
         Materialize.showStaggeredList($(el));
       } },
       {selector: '#hangman-pic', offset: 200, callback: function(el) {
-        Materialize.fadeInImage($(el));
-      } },
-      {selector: '#bracketbuilder-list', offset: 150, callback: function(el) {
-        Materialize.showStaggeredList($(el));
-      } },
-      {selector: '#bracketbuilder-pic', offset: 200, callback: function(el) {
         Materialize.fadeInImage($(el));
       } },
       {selector: '.resume-links', offset: 150, callback: function(el) {
